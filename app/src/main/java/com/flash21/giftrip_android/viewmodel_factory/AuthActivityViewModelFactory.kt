5674e13c2a16d5c31cbd9bf2da3c9fa23d23094f
YONGJAEEE <56459713+YONGJAEEE@users.noreply.github.com>
2021-06-11 10:52:37 +0900

    Setting AuthActivityViewModel

---
+++ app/src/main/java/com/flash21/giftrip_android/viewmodel_factory/AuthActivityViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.flash21.giftrip_android.viewmodel_factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.flash21.giftrip_android.viewmodel.MainActivityViewModel
+
+class AuthActivityViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainActivityViewModel::class.java)){
+            return MainActivityViewModel() as T
+        }
+        throw IllegalArgumentException("Unknown View Model Class")
+    }
+}
